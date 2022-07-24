24009c4a5797d0c3f46b2099a353a2cce2b65a70
Zheng Ye <zhengye0118@gmail.com>
2022-07-24 16:02:59 -0500

    add Async Thunk functionality to fetch from Fake Rest API server site
    Also added functionality to get posts, post new posts, update existing posts, and delete posts

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { fetchUsers } from './features/users/usersSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { fetchPosts } from './features/posts/postsSlice';
+
+store.dispatch(fetchPosts());
+store.dispatch(fetchUsers());
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,7 +18,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
